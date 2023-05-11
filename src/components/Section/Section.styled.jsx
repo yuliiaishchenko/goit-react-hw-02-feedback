ac9ffd6728373d9a7d85864a45105b4764d895f7
yuliiaishchenko <romashka.ukr@gmail.com>
2023-05-11 16:27:54 +0300

    styled.section

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,6 +1,24 @@
 import styled from "@emotion/styled";
 
-export const SectionContainer = styled.section``;
+export const SectionContainer = styled.section`
+    max-width: 370px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: linen;
+    border-radius: 15px;
+    margin-bottom: 15px;
+    padding: 10px;
+`;
 
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+    text-align: center;
+    color: #d54d7b;
+    font-family: "Libre Baskerville", serif;
+    font-size: 24px;
+    font-style: italic;
+    font-weight: normal;
+    margin-bottom: 0px;
+    text-align: center;
+    text-shadow: 0 1px 1px gray;
+`;
